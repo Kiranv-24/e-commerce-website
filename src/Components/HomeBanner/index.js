f4a97bbefe4a7afd667736aa4534882767d69cc5
KIRAN V <kiranv24042004@gmail.com>
2024-10-10 18:56:13 +0530

    Modified-category done

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -1,9 +1,31 @@
 import React from "react";
 import Slider from "react-slick";
-import Banner1 from "../../assets/images/scroll1.jpg";
-import Banner2 from "../../assets/images/scroll2.jpg";
-import Banner3 from "../../assets/images/scroll3.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import BannerGif1 from "../../assets/images/scrollGif1.gif"; // Import your GIF here
+import BannerGif2 from "../../assets/images/scrollGif2.gif"; // Import your GIF here
 import CategoryDisplay from "../../Components/Headers/CategoryDisplay";
+
+// Custom Next Arrow component
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="slick-arrow slick-next" onClick={onClick}>
+      <FontAwesomeIcon icon={faArrowRight} />
+    </div>
+  );
+};
+
+// Custom Prev Arrow component
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="slick-arrow slick-prev" onClick={onClick}>
+      <FontAwesomeIcon icon={faArrowLeft} />
+    </div>
+  );
+};
+
 const HomeBanner = () => {
   var settings = {
     dots: true,
@@ -11,28 +33,29 @@ const HomeBanner = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true, // Autoplay the slides
-    autoplaySpeed: 3000, // 3 seconds per slide
-    arrows: true, // Enable navigation arrows
-    adaptiveHeight: true, // Adjust height based on content
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    nextArrow: <NextArrow />, // Use custom Next arrow
+    prevArrow: <PrevArrow />, // Use custom Prev arrow
+    adaptiveHeight: true,
   };
 
   return (
     <div className="HomeBannerSection">
-      <Slider {...settings}>
-        <div className="item">
-          <img src={Banner1} className="w-100" alt="Banner 1" />
+      <div className="banner-content">
+        <div className="category-block">
+          <CategoryDisplay />
         </div>
-        <div className="item">
-          <img src={Banner2} className="w-100" alt="Banner 2" />
-        </div>
-        <div className="item">
-          <img src={Banner3} className="w-100" alt="Banner 3" />
-        </div>
-        {/* Add more banners as needed */}
-      </Slider>
-      <div className="CategoryDisplay">
-        <CategoryDisplay />
+        <Slider {...settings} className="gif-slider">
+          <div className="item">
+            <img src={BannerGif1} className="w-100" alt="Banner GIF 1" />
+          </div>
+          <div className="item">
+            <img src={BannerGif2} className="w-100" alt="Banner GIF 1" />
+          </div>
+          {/* You can add more GIFs here if needed */}
+        </Slider>
       </div>
     </div>
   );
