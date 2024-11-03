bec3920a7c6b704cdfb9358893eaa18364604b78
KIRAN V <kiranv24042004@gmail.com>
2024-11-03 18:25:14 +0530

    bug

--- client/src/Pages/ProductDetails/index.js
+++ client/src/Pages/ProductDetails/index.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Button } from "@mui/material";
 import InnerImageZoom from "react-inner-image-zoom";
 import { FaMinus, FaPlus } from "react-icons/fa";
-import { fetchDataFromApi, postData,deleteData } from "../../api";
+import { fetchDataFromApi, postData, deleteData } from "../../api";
 import { useParams, useNavigate } from "react-router-dom";
 import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
 import "../../Css-files/ProductDetails.css";
 import CartIcon from "../../Components/Headers/Cart-icon";
-import { Toaster, toast } from 'react-hot-toast'; 
+import { Toaster, toast } from "react-hot-toast";
 import MyContext from "../../Mycontext/index.js";
 
 const ProductDetail = () => {
   const [quantity, setQuantity] = useState(1);
-  const { setCartCount,cartCount } = useContext(MyContext);
+  const { setCartCount, cartCount } = useContext(MyContext);
   const [proData, setProData] = useState(null);
   const { id } = useParams();
   const navigate = useNavigate();
@@ -32,14 +32,12 @@ const ProductDetail = () => {
   const handleQuantityChange = (event) => {
     const value = event.target.value;
 
-    
     if (value === "" || /^[1-9]\d*$/.test(value)) {
       setQuantity(value === "" ? "" : parseInt(value));
     }
   };
 
   const handleBlur = () => {
-   
     if (quantity === "" || quantity < 1) {
       setQuantity(1);
     }
@@ -63,11 +61,11 @@ const ProductDetail = () => {
 
   const addToCart = async () => {
     if (!username) {
-      toast.error("Login to your account"); 
+      toast.error("Login to your account");
       navigate("/login");
       return;
     }
-    
+
     const cartItem = {
       productId: id,
       quantity: quantity,
@@ -76,65 +74,68 @@ const ProductDetail = () => {
 
     const response = await postData("/api/cart/add", cartItem);
     if (response) {
-      const res=await fetchDataFromApi(`/api/cart/${username}`);
+      const res = await fetchDataFromApi(`/api/cart/${username}`);
       setCartCount(res.cartitems.length);
 
-      toast.success("Item added to cart"); 
+      toast.success("Item added to cart");
     }
-    <CartIcon />;
+    // <CartIcon />;
   };
 
   if (!proData) {
     return <div>Loading...</div>;
   }
- const goBack = () => {
-     window.location.replace("/");
+  const goBack = () => {
+    window.location.replace("/");
   };
-const buyNow = async () => {
-  if (!username) {
-    toast.error("Login to your account");
-    navigate("/login");
-    return;
-  }
-
-  try {
-   
-    await addToCart(); 
-      const result=await fetchDataFromApi(`/api/cart/${username}`);
-      setCartCount(result.cartitems.length);
-   
-    const res = await fetchDataFromApi(`/api/Cart/${username}`); 
-      // console.log(res);
-   
-    const response = await deleteData(`/api/checkout/clear-shipping/${username}`);
-    
-    // console.log(response.status);
-    if (response.success) {
-      navigate(`/Checkout?ref=nav_cart`, {
-        state: {
-          cartItems: res.cartitems, 
-          quantities: [1]
-        },
-      });
+  const buyNow = async () => {
+    if (!username) {
+      toast.error("Login to your account");
+      navigate("/login");
+      return;
     }
-  } catch (error) {
-    toast.error("Error in sending data", error);
-  }
-};
 
+    try {
+      await addToCart();
+      const result = await fetchDataFromApi(`/api/cart/${username}`);
+      setCartCount(result.cartitems.length);
 
+      const res = await fetchDataFromApi(`/api/Cart/${username}`);
+      // console.log(res);
+
+      const response = await deleteData(
+        `/api/checkout/clear-shipping/${username}`
+      );
+
+      // console.log(response.status);
+      if (response.success) {
+        navigate(`/Checkout?ref=nav_cart`, {
+          state: {
+            cartItems: res.cartitems,
+            quantities: [1],
+          },
+        });
+      }
+    } catch (error) {
+      toast.error("Error in sending data", error);
+    }
+  };
 
   return (
     <div className="dialog-content">
       <div className="go-back-button-container">
-  <Button onClick={goBack} className="go-back-button">
-    Go Back
-  </Button>
-</div>
+        <Button onClick={goBack} className="go-back-button">
+          Go Back
+        </Button>
+      </div>
       <div className="ProductDetail">
         <div className="ProductDetail-image">
           {windowWidth < 768 ? (
-            <img src={proData.images[0]} alt={proData.name} style={{ maxWidth: "80%", borderRadius: "10px" }} />
+            <img
+              src={proData.images[0]}
+              alt={proData.name}
+              style={{ maxWidth: "80%", borderRadius: "10px" }}
+            />
           ) : (
             <InnerImageZoom
               src={proData.images[0]}
@@ -156,7 +157,11 @@ const buyNow = async () => {
             >
               ADD TO CART
             </Button>
-            <Button className="buy-now" style={{ color: "black" }} onClick={buyNow}>
+            <Button
+              className="buy-now"
+              style={{ color: "black" }}
+              onClick={buyNow}
+            >
               BUY NOW
             </Button>
             <div className="quantity-group">
@@ -167,7 +172,7 @@ const buyNow = async () => {
                 type="text"
                 value={quantity}
                 onChange={handleQuantityChange}
-                onBlur={handleBlur} 
+                onBlur={handleBlur}
                 className="quantity-input"
               />
               <Button className="quantity-btn" onClick={incrementQuantity}>
